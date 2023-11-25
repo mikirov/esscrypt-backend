808fba9f9f5f5906712b95415eaa6d9c975ff485
Mihail Kirov <mihail.kirov@limechain.tech>
2023-11-25 18:07:28 +0200

    WIP:

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Load environment variables from .env
-# if [ -f .env ]; then
-#     export $(cat .env | xargs)
-# fi
+Load environment variables from .env
+if [ -f .env ]; then
+    export $(cat .env | xargs)
+fi
 
 # Install project dependencies from requirements.txt
 pip install -r requirements.txt
@@ -11,8 +11,5 @@ pip install -r requirements.txt
 # Collect static files
 python3.9 manage.py collectstatic --noinput
 
-python manage.py createsuperuser \
-    --noinput \
-    --username $DJANGO_SUPERUSER_USERNAME \
-    --email $DJANGO_SUPERUSER_EMAIL
+python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
 
