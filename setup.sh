879ef08fb283a40810180d9662ba84f2b6f1afad
Mihail Kirov <mihail.kirov@limechain.tech>
2023-11-25 18:47:41 +0200

    WIP

--- setup.sh
+++ setup.sh
@@ -10,7 +10,5 @@ pip install -r requirements.txt
 
 # Collect static files
 python3.9 manage.py collectstatic --noinput
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
 python3.9 manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
 
